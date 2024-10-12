1b2a5e2b3da40588459e2d9b1a0af46bd8894e08
Shivam Bhanu <shivambhanu6924@gmail.com>
2024-10-12 13:47:55 +0530

    Initiated setup

---
+++ src/main/java/com/backend/pdf_generator/PdfGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pdf_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGeneratorApplication.class, args);
+	}
+
+}
